95e79e785812644038d11b0d9060979e000e43f6
Henning Pohlmeyer <hpohlmeyer@users.noreply.github.com>
2025-08-27 09:43:11 +0200

    Delete range slider

--- packages/range-slider/src/App.tsx
+++
@@ -1,9 +0,0 @@
-import _FieldPlugin from './components/FieldPlugin'
-import FieldPluginExample from './components/FieldPluginExample'
-import { FunctionComponent } from 'react'
-
-const App: FunctionComponent = () => {
-  return <FieldPluginExample />
-}
-
-export default App
--- packages/range-slider/src/createRootElement.ts
+++
@@ -1,6 +0,0 @@
-export const createRootElement = (id?: string): HTMLElement => {
-  // In production, `#app` may or may not exist.
-  const rootElement = document.createElement('div')
-  rootElement.id = id ?? 'app'
-  return rootElement
-}
